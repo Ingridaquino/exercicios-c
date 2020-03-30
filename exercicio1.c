e9b43c34de4803dc65d8498f0c7ce7bcfcc8ef75
Ingrid Aquino <KlintonLee92@gmail.com>
2020-03-30 18:30:28 -0300

    exercicio 1 finalizado

---
+++ exercicio1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+
+  int a, b, x;
+
+  printf("Digite o primeiro numero: ");
+  scanf("%d", &a);
+  printf("Digite o segundo numero: ");
+  scanf("%d", &b);
+
+  x = a + b;
+
+
+  printf("X = %d\n",x);
+
+
+  return 0; 
+
+
+}
