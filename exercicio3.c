380712019559abd924d2f2cc058bc68f2b773720
Ingrid Aquino <ingridaquinosoares@live.com>
2020-03-30 18:59:24 -0300

    Exercicio 3 finalizado

---
+++ exercicio3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+  int a, b, soma;
+
+  printf("Digite um numero: ");
+  scanf("%d", &a);
+  printf("Digite mais um numero: ");
+  scanf("%d", &b);
+
+  soma = a + b; 
+
+  printf("SOMA = %d\n", soma);
+
+
+
+
+
+  return 0;
+}
