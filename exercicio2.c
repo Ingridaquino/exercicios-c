43ba174119229fde03faba63328ab25bdbd50bef
Ingrid Aquino <ingridaquinosoares@live.com>
2020-03-30 18:43:51 -0300

    exercicio 2 finalizado

---
+++ exercicio2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+  double area, raio;
+
+  printf("Digite um valor do raio: ");
+  scanf("%lf", &raio);
+
+  area = 3.14159 * (raio * raio);
+
+  printf("A=%.4lf\n", area);
+
+
+
+
+
+  return 0;
+}
